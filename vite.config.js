d9c382d5451c065a710226294241348c2bcf6b72
cylee323071@gmail.com <cylee323071@gmail.com>
2025-09-03 21:54:22 +0900

    메인 페이지 및 영화 상세 페이지 레이아웃 구성

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },
